cc683e05ccd5ca5e228ae8cdfc744a1a15964f06
djamelall22 <alphamodeling.contact@gmail.com>
2020-06-16 16:47:21 +0200

    PrototypePrototype
    git commit -m PrototypePrototype

--- Object-oriented Programming/02/index.js
+++ Object-oriented Programming/02/index.js
@@ -137,6 +137,30 @@
 
 // -----------------------------Prototype-------------------------------
 
+// function User(email, name) {
+//   this.email = email;
+//   this.name = name;
+//   this.online = false;
+// }
+
+// User.prototype.login = function () {
+//   this.online = true;
+//   console.log(this.email, 'has logged in');
+// };
+
+// User.prototype.logout = function () {
+//   this.online = false;
+//   console.log(this.email, 'has logged out');
+// };
+
+// var userOne = new User('ryu@ninjas.com', 'Ryu');
+// var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+
+// console.log(userOne);
+// userTwo.login();
+
+// -------------------------Prototype Inheritance-----------------------------------
+
 function User(email, name) {
   this.email = email;
   this.name = name;
@@ -153,8 +177,20 @@ User.prototype.logout = function () {
   console.log(this.email, 'has logged out');
 };
 
+function Admin(...args) {
+  User.apply(this, args);
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function (u) {
+  users = users.filter((user) => {
+    return user.email != u.email;
+  });
+};
+
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+var admin = new Admin('shaun@ninjas.com', 'Shaun');
 
-console.log(userOne);
-userTwo.login();
+var users = [userOne, userTwo, admin];
