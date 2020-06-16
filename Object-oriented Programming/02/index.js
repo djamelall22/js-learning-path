b9921dd459b103b5556987e55f2b6291e644dffe
djamelall22 <alphamodeling.contact@gmail.com>
2020-06-16 14:15:39 +0200

    under the hood

--- Object-oriented Programming/02/index.js
+++ Object-oriented Programming/02/index.js
@@ -78,42 +78,59 @@
 
 // --------------------Class Inheritance---------------------
 
-class User {
-  constructor(email, name) {
-    this.email = email;
-    this.name = name;
-    this.score = 0;
-  }
-  login() {
-    console.log(this.email, 'just logged in');
-    return this;
-  }
-  logout() {
-    console.log(this.email, 'just logged out');
-    return this;
-  }
-  updateScore() {
-    this.score++;
-    console.log(this.email, 'score is now', this.score);
-    return this;
-  }
-}
+// class User {
+//   constructor(email, name) {
+//     this.email = email;
+//     this.name = name;
+//     this.score = 0;
+//   }
+//   login() {
+//     console.log(this.email, 'just logged in');
+//     return this;
+//   }
+//   logout() {
+//     console.log(this.email, 'just logged out');
+//     return this;
+//   }
+//   updateScore() {
+//     this.score++;
+//     console.log(this.email, 'score is now', this.score);
+//     return this;
+//   }
+// }
 
-class Admin extends User {
-  deleteUser(user) {
-    users = users.filter((u) => {
-      return u.email != user.email;
-    });
-  }
+// class Admin extends User {
+//   deleteUser(user) {
+//     users = users.filter((u) => {
+//       return u.email != user.email;
+//     });
+//   }
+// }
+
+// var userOne = new User('ryu@ninjas.com', 'Ryu');
+// var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+// var admin = new Admin('shaun@ninjas.com', 'Shaun');
+
+// var users = [userOne, userTwo, admin];
+
+// // admin.deleteUser(userTwo);
+// userTwo.deleteUser(userOne); // won't work
+
+// console.log(users);
+
+// -----------------------------------------under the hood------------------------------
+
+function User(email, name) {
+  this.email = email;
+  this.name = name;
+  this.online = false;
+  this.login = function () {
+    console.log(this.email, 'has logged in');
+  };
 }
 
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
-var admin = new Admin('shaun@ninjas.com', 'Shaun');
 
-var users = [userOne, userTwo, admin];
-
-// admin.deleteUser(userTwo);
-userTwo.deleteUser(userOne); // won't work
-
-console.log(users);
+console.log(userOne);
+userTwo.login();
